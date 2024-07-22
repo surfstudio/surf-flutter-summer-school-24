8c7217315f779e6fdf7f6a5d7500ad8e4b5b118d
vladoo <vlad.borisenkov@gmail.com>
2024-07-22 21:24:27 +0300

    add solution for task 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,116 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: PhotoViewer(),
+    );
+  }
+}
+
+class PhotoViewer extends StatefulWidget {
+  const PhotoViewer({super.key});
+
+  @override
+  _PhotoViewerState createState() => _PhotoViewerState();
+}
+
+class _PhotoViewerState extends State<PhotoViewer> {
+  final List<String> images = [
+    'assets/images/1.jpeg',
+    'assets/images/2.jpeg',
+    'assets/images/3.jpeg',
+    'assets/images/4.jpeg',
+    'assets/images/5.jpeg',
+  ];
+
+  final PageController _controller = PageController(
+    viewportFraction: 0.85,
+  );
+
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      setState(() {
+        _currentPage = _controller.page!.round();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            // Действие на нажатие кнопки "Назад"
+          },
         ),
+        title: const Text('Title'),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Text(
+                '${_currentPage + 1}/${images.length}',
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: PageView.builder(
+        controller: _controller,
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          double scale = _currentPage == index ? 1.0 : 0.9;
+          return TweenAnimationBuilder(
+            duration: Duration(milliseconds: 350),
+            tween: Tween(begin: scale, end: scale),
+            builder: (context, value, child) {
+              return Transform.scale(
+                scale: value,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0, vertical: 30.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Image.asset(
+                          images[index],
+                          fit: BoxFit.cover,
+                        ),
+                        if (_currentPage != index)
+                          BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                            child: Container(
+                              color: Colors.black.withOpacity(0),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
