c60b4cd02e2c972642356e763d4605f9c4695022
SugarHedgehog <sneginca.cat@gmail.com>
2024-07-22 23:13:27 +0300

    day-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const AppBarApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: AppBarExample(),
+    );
+  }
+}
+
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("05.06.2023"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
+        actions: [
+          Container(
+              child: const Text.rich(
+                TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: '13',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                        TextSpan(
+                        text: '/37',)
+                        //style: TextStyle(fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ))
+        ],
+      ),
+      body: Center(
+        child: Container(
+            decoration: BoxDecoration(border: Border.all()),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(40.0),
+                child: Image.asset(
+                  "assets/images/Mahou_Shoujo_Madoka_Magika.jpg",
+                  scale: 0.5,
+                ))),
       ),
     );
   }
