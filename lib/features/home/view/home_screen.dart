51fde233a9941e567faac2159f706e1766c73dde
sahland <sahland@mail.ru>
2024-07-22 23:41:45 +0300

    create home screen

---
+++ lib/features/home/view/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:surf_flutter_summer_school_24/router/router.dart';
+
+@RoutePage()
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return const AutoTabsRouter(
+      duration: const Duration(microseconds: 250),
+      routes: [
+        TapeRoute()
+      ],
+    );
+  }
+}
