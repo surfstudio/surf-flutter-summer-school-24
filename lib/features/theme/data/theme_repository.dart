7b9572a54558f8acd4c4b858144fcd06a4e21928
sahland <sahland@mail.ru>
2024-07-23 20:49:51 +0300

    create theme data

---
+++ lib/features/theme/data/theme_repository.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:surf_flutter_summer_school_24/theme/theme.dart';
+import 'package:surf_flutter_summer_school_24/theme/theme_storage.dart';
+
+class ThemeRepository {
+  final ThemeStorage _themeStorage;
+
+  ThemeRepository({
+    required ThemeStorage themeStorage,
+  }) : _themeStorage = themeStorage;
+
+  Future<void> setThemeMode(
+    ThemeMode newThemeMode,
+  ) async {
+    await _themeStorage.saveThemeMode(mode: newThemeMode);
+  }
+
+  ThemeMode? getThemeMode() {
+    return _themeStorage.getThemeMode();
+  }
+}
