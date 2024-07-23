84194c7c149c58618d896298e9df5631d5dc1681
sahland <sahland@mail.ru>
2024-07-23 20:55:21 +0300

    update main project files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:surf_flutter_summer_school_24/features/features.dart';
 import 'package:surf_flutter_summer_school_24/postogram_app.dart';
+import 'package:surf_flutter_summer_school_24/theme/theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const PostogramApp());
+  final prefs = await SharedPreferences.getInstance();
+  final themeStorage = ThemeStorage(prefs: prefs);
+  final themeRepository = ThemeRepository(
+    themeStorage: themeStorage,
+  );
+  final themeController = ThemeController(themeRepository: themeRepository);
+
+  runApp(PostogramApp(themeController: themeController));
 }
-
-
-
-
--- lib/postogram_app.dart
+++ lib/postogram_app.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:surf_flutter_summer_school_24/features/features.dart';
 import 'package:surf_flutter_summer_school_24/router/router.dart';
+import 'package:surf_flutter_summer_school_24/uikit/uikit.dart';
 
 class PostogramApp extends StatefulWidget {
-  const PostogramApp({super.key});
+  const PostogramApp({required this.themeController, super.key});
+
+  final ThemeController themeController;
 
   @override
   State<PostogramApp> createState() => _PostogramAppState();
@@ -13,15 +18,19 @@ class _PostogramAppState extends State<PostogramApp> {
 
   @override
   Widget build(BuildContext context) {
-    const primaryColor = Colors.black;
-    return MaterialApp.router(
-      theme: ThemeData(
-        primaryColor: primaryColor,
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-        useMaterial3: true
+    return ThemeInherited(
+      themeController: widget.themeController,
+      child: ThemeBuilder(
+        builder: (_, themeMode) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            theme: AppThemeData.lightThem,
+            darkTheme: AppThemeData.darkTheme,
+            themeMode: themeMode,
+            routerConfig: _router.config(),
+          );
+        },
       ),
-      routerConfig: _router.config(),
     );
   }
 }
