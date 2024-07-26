ea91a2986038ef9007505992f4c3df7e1f65b49c
Artem <razhev.2003@mail.ru>
2024-07-26 15:38:11 +0300

    end result

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+import 'image_grid.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  void toggleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,117 +26,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ImageCarouselPage(
-        imageUrls: [
-          'https://i.pinimg.com/originals/58/c6/c2/58c6c2ca662f71f6e164de46e4f9885d.jpg',
-          'https://i.pinimg.com/originals/1a/76/ee/1a76eedb59116d6bde09c32fb6b1a8bd.jpg',
-          'https://avatars.mds.yandex.net/i?id=073d3664d83f19c1ca766153c7d4cfaf3963aece-5042632-images-thumbs&n=13',
-        ],
-      ),
-    );
-  }
-}
-
-class ImageCarouselPage extends StatefulWidget {
-  final List<String> imageUrls;
-
-  ImageCarouselPage({required this.imageUrls});
-
-  @override
-  _ImageCarouselPageState createState() => _ImageCarouselPageState();
-}
-
-class _ImageCarouselPageState extends State<ImageCarouselPage> {
-  int _currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Align(
-          alignment: Alignment.topCenter,
-          child: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {}, // Leave it empty to make it non-functional
-          ),
-        ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Center(
-                child: Text(
-                  '21.05.2023',
-                  style: TextStyle(),
-                ),
-              ),
-            ),
-            Text(
-              '${_currentIndex + 1}/${widget.imageUrls.length}',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-        centerTitle: true,
-      ),
-      body: CarouselSlider.builder(
-        itemCount: widget.imageUrls.length,
-        itemBuilder: (context, index, realIndex) {
-          final imageUrl = widget.imageUrls[index];
-          return ShaderMask(
-            blendMode: BlendMode.srcOver,
-            shaderCallback: (bounds) {
-              return LinearGradient(
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-                colors: [
-                  index == _currentIndex ? Colors.transparent : Colors.black12,
-                  Colors.transparent,
-                ],
-                stops: [0.5, 0.5001],
-                tileMode: TileMode.clamp,
-              ).createShader(bounds);
-            },
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 8.0),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16.0),
-                child: Image.network(
-                  imageUrl,
-                  fit: BoxFit.cover,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  },
-                ),
-              ),
-            ),
-          );
-        },
-        options: CarouselOptions(
-          height: 800.0,
-          aspectRatio: 16 / 9,
-          viewportFraction: 0.8,
-          initialPage: 0,
-          enableInfiniteScroll: true,
-          reverse: false,
-          autoPlay: true,
-          autoPlayInterval: Duration(seconds: 3),
-          autoPlayAnimationDuration: Duration(milliseconds: 800),
-          autoPlayCurve: Curves.fastOutSlowIn,
-          enlargeCenterPage: true,
-          scrollDirection: Axis.horizontal,
-          onPageChanged: (index, reason) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-        ),
+      darkTheme: ThemeData.dark(),
+      themeMode: _themeMode,
+      home: ImageGridPage(
+        toggleTheme: toggleTheme,
+        isDarkTheme: _themeMode == ThemeMode.dark,
       ),
     );
   }
